e11206223675ea8ab26782c40c1eda4e1a19eff1
Nguyen Tri Nghia <nghiant@mailbox.org>
2026-09-12 15:33:49 +0800

    Solve pearson-correlation

---
+++ pearson-correlation/pearson-correlation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def pearson_correlation(X: list) -> np.ndarray:
+    """
+    Returns the correlation matrix as a NumPy array.
+    """
+    # Write code here
+    X = np.array(X)
+    X_c = X - np.mean(X, axis=0)
+    variance = X_c.T @ X_c
+    deviation = np.sqrt(np.sum(np.square(X_c), axis=0, keepdims=True))
+    print(variance)
+    print(deviation.T @ deviation)
+    return variance / (deviation.T @ deviation)
